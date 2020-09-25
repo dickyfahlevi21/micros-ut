3da28d88aaf2f6b254cd06f3bcc93e20a9bd74ef
Husen Efendi <husenef@gmail.com>
2020-09-25 10:16:04 +0700

    add servicer email

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # micros-ut
+
+
+## How To
+* clone from [this repo](https://github.com/dickyfahlevi21/backend-simple-wms) and set folder name with service-name:
+    ```bash 
+    $ git clone git@github.com:dickyfahlevi21/backend-simple-wms.git service-email
+
+    $ cd service-email 
+
+    $ rm -rf .git ## remove clone url 
+    ```
+
+* create `Dockerfile` to create docker images
+* push to docker hub
---
+++ service-email/app.js
@@ -0,0 +1,79 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+// untuk upload ke cloudinary
+const fileUpload = require("express-fileupload");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// const userRoute = require("./routes/user");
+// const productRoute = require("./routes/product");
+// const productInRoute = require("./routes/product_in");
+// const authRoute = require("./routes/auth");
+// const productOutRoute = require("./routes/product_out");
+// const reportRoute = require("./routes/print");
+// const auth = require("./middleware/AuthMiddleware");
+const taskScheduler = require("./helpers/taskScheduler");
+
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "http://localhost:8081",
+  "http://localhost",
+  /\.heroku\.com$/,
+];
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//cors
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       console.log({ origin, ada: whitelist.indexOf(origin) });
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
+
+// untuk cloudinary
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/halo", (req, res) => {
+  res.json({ hello: "email" });
+});
+// app.use("/api/v1/user", auth, userRoute);
+// app.use("/api/v1/product", auth, productRoute);
+// app.use("/api/v1/in", auth, productInRoute);
+// app.use("/api/v1/auth", authRoute);
+// app.use("/api/v1/out", auth, productOutRoute);
+// app.use("/api/v1/print", auth, reportRoute);
+
+app.get("/send-email",(req,res)=>{
+  res.send("email send");
+})
+
+app.get("/report",(req,res)=>{
+  res.send("email send");
+})
+taskScheduler();
+
+app.listen(port, () => console.log("Listened on port " + port));
